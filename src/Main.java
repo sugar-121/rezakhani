40a15e90c0e3a589dab23fd9ccb230e97618ce31
imhosseinzadeh <imhosseinzadeh@gmail.com>
2025-11-27 10:05:31 +0330

    commit my changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 void main() {
     IO.println(String.format("Hello and welcome!"));
-
+    System.out.println(String.format("Hello World!"));
 
     System.out.println("Hello");
     for (int i = 1; i <= 5; i++) {
